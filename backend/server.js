4731d61e192eed4f39b7f1b3a97d12e3afcd2c79
ibnegufran <ibnegufran75074@gmail.com>
2025-07-10 16:20:45 +0530

    new

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express=require('express')
 const mongoose=require('mongoose');
-const cors=require('cors')
-require('dotenv').config();
 const userRouter=require('./router/userRouter');
 const DataRouter=require("./router/DataRouter")
 const app =express();
+const cors=require('cors')
+require('dotenv').config();
 
 app.use(cors({
   origin: "https://adidata.netlify.app", // your Netlify URL without trailing slash
